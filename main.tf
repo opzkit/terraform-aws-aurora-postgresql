71047d9eb05b80279a60f300a6d8b63e8a064192
Peter Svensson <peter@sparetimecoders.com>
2025-04-07 23:17:27 +0200

    feat: enhance RDS configuration and security group settings
    Update the RDS cluster and instance configurations to include 
    deletion protection and CloudWatch log exports. Improve the 
    security group by adding descriptions for ingress and egress 
    rules and ensure the PostgreSQL traffic is clearly defined. 
    Introduce new variables for deletion protection and CloudWatch 
    log exports, enhancing the configuration's security and 
    monitoring capabilities.

--- main.tf
+++ main.tf
@@ -11,10 +11,12 @@ resource "aws_db_subnet_group" "default" {
 }
 
 resource "aws_security_group" "allow_postgres" {
-  vpc_id = var.vpc.id
-  name   = "allow-postgresql-${var.identifier}"
+  name        = "allow-postgresql-${var.identifier}"
+  vpc_id      = var.vpc.id
+  description = "allow traffic to postgres"
 
   egress {
+    description = "allow outgoing traffic from postgres"
     from_port   = 0
     protocol    = "-1"
     to_port     = 0
@@ -22,6 +24,7 @@ resource "aws_security_group" "allow_postgres" {
   }
   # This is probably secure enough - can be removed and setup externally if needed...
   ingress {
+    description = "allow traffic to postgres on port 5432"
     from_port   = 5432
     protocol    = "TCP"
     to_port     = 5432
@@ -30,17 +33,23 @@ resource "aws_security_group" "allow_postgres" {
 }
 
 resource "aws_rds_cluster" "default" {
-  cluster_identifier      = var.identifier
-  engine                  = "aurora-postgresql"
-  engine_version          = var.postgresql_version
-  engine_mode             = "provisioned"
-  availability_zones      = var.zones
-  database_name           = var.db_name
-  master_username         = var.master_username
-  master_password         = local.password
-  backup_retention_period = 14
-  preferred_backup_window = "03:00-05:00"
-  db_subnet_group_name    = aws_db_subnet_group.default.name
+  #checkov:skip=CKV_AWS_139: Deletion protection enabled by variable
+  deletion_protection = var.deletion_protection
+  #checkov:skip=CKV_AWS_162: IAM authentication disabled
+  iam_database_authentication_enabled = false
+  #checkov:skip=CKV_AWS_324: Log capture enabled by variable
+  enabled_cloudwatch_logs_exports = var.enabled_cloudwatch_logs_exports ? ["postgresql", "upgrade"] : []
+  cluster_identifier              = var.identifier
+  engine                          = "aurora-postgresql"
+  engine_version                  = var.postgresql_version
+  engine_mode                     = "provisioned"
+  availability_zones              = var.zones
+  database_name                   = var.db_name
+  master_username                 = var.master_username
+  master_password                 = local.password
+  backup_retention_period         = 14
+  preferred_backup_window         = "03:00-05:00"
+  db_subnet_group_name            = aws_db_subnet_group.default.name
   vpc_security_group_ids = [
     aws_security_group.allow_postgres.id
   ]
@@ -51,9 +60,11 @@ resource "aws_rds_cluster" "default" {
   kms_key_id                      = var.kms_key_arn
   allow_major_version_upgrade     = var.allow_major_version_upgrade
   apply_immediately               = var.apply_immediately
+  copy_tags_to_snapshot           = true
 }
 
 resource "aws_rds_cluster_instance" "writer" {
+  #checkov:skip=CKV_AWS_118: Monitoring enabled by variable
   cluster_identifier                    = aws_rds_cluster.default.cluster_identifier
   identifier                            = "${var.identifier}-writer"
   instance_class                        = var.writer_instance_type
@@ -65,6 +76,7 @@ resource "aws_rds_cluster_instance" "writer" {
   performance_insights_enabled          = true
   performance_insights_retention_period = var.performance_insights_retention_period
   ca_cert_identifier                    = var.ca_cert_identifier
+  auto_minor_version_upgrade            = true
 }
 
 resource "aws_rds_cluster_parameter_group" "cluster_parameters" {
